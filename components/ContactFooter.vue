b3f2149ad79f6d8b1182c0b50b74dec131894d2d
keeganhanesboise <keeganhanes5@gmail.com>
2025-04-22 16:31:01 -0700

    created contact footer component

---
+++ components/ContactFooter.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+defineProps<{
+  currentDate: string | undefined;
+  instagramIcon: string;
+  discordIcon: string;
+  setInstagramIcon: (src: string) => void;
+  setDiscordIcon: (src: string) => void;
+  customClass?: string;
+}>();
+</script>
+
+<template>
+  <div class="footer-container" :class="customClass">
+    <div>
+      <p class="footer-text copy-right-text">
+        {{ `© ${currentDate} One Haven. All rights reserved.` }}
+      </p>
+      <a class="footer-text" href="/privacy">Privacy Policy</a>
+    </div>
+    <div class="social-container">
+      <a
+        class="social-icon-container"
+        href="https://www.instagram.com/one_haven5956/"
+        target="_blank"
+        @blur="setInstagramIcon('/img/social-icons/Instagram_Glyph_White.svg')"
+        @focus="
+          setInstagramIcon('/img/social-icons/Instagram_Glyph_Gradient.svg')
+        "
+        @mouseenter="
+          setInstagramIcon('/img/social-icons/Instagram_Glyph_Gradient.svg')
+        "
+        @mouseleave="
+          setInstagramIcon('/img/social-icons/Instagram_Glyph_White.svg')
+        ">
+        <img
+          alt="Link to the One Haven Instagram"
+          class="social-icon"
+          :src="instagramIcon" />
+      </a>
+      <a
+        class="social-icon-container"
+        href="https://discord.gg/FWnQzgjr"
+        target="_blank"
+        @blur="setDiscordIcon('/img/social-icons/Discord-Symbol-White.svg')"
+        @focus="setDiscordIcon('/img/social-icons/Discord-Symbol-Blurple.svg')"
+        @mouseenter="
+          setDiscordIcon('/img/social-icons/Discord-Symbol-Blurple.svg')
+        "
+        @mouseleave="
+          setDiscordIcon('/img/social-icons/Discord-Symbol-White.svg')
+        ">
+        <img
+          alt="Link to join the One Haven Discord"
+          class="social-icon"
+          :src="discordIcon" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+p,
+a {
+  color: white;
+}
+.footer-text {
+  font-size: 14px;
+  margin: 0;
+}
+.copy-right-text {
+  padding-bottom: 5px;
+}
+.footer-container {
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  gap: 15px;
+}
+.social-container {
+  gap: 15px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+.social-icon {
+  height: 30px;
+}
+@media (max-width: 768px) {
+  .social-container {
+    gap: 10px;
+  }
+}
+</style>
--- components/ContactUs.vue
+++ components/ContactUs.vue
@@ -41,67 +41,13 @@ const setInstagramIcon = (src: string) => {
             referrerpolicy="no-referrer-when-downgrade"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3104.0396103642324!2d-104.7196247226171!3d38.91432037172041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87134931a0d1dbad%3A0xcd57e855790dcef5!2sOne%20Haven!5e0!3m2!1sen!2sus!4v1743550471671!5m2!1sen!2sus"
             title="map" />
-          <div
-            class="is-displayed-desktop-flex-l footer-container margin-top-10px">
-            <div>
-              <p class="footer-text copy-right-text">
-                {{ `© ${currentDate} One Haven. All rights reserved.` }}
-              </p>
-              <a class="footer-text" href="/privacy">Privacy Policy</a>
-            </div>
-            <div class="social-container">
-              <a
-                class="social-icon-container"
-                href="https://www.instagram.com/one_haven5956/"
-                target="_blank"
-                @blur="
-                  setInstagramIcon(
-                    '/img/social-icons/Instagram_Glyph_White.svg'
-                  )
-                "
-                @focus="
-                  setInstagramIcon(
-                    '/img/social-icons/Instagram_Glyph_Gradient.svg'
-                  )
-                "
-                @mouseenter="
-                  setInstagramIcon(
-                    '/img/social-icons/Instagram_Glyph_Gradient.svg'
-                  )
-                "
-                @mouseleave="
-                  setInstagramIcon(
-                    '/img/social-icons/Instagram_Glyph_White.svg'
-                  )
-                ">
-                <img
-                  alt="Link to the One Haven Instagram"
-                  class="social-icon"
-                  :src="instagramIcon" />
-              </a>
-              <a
-                class="social-icon-container"
-                href="https://discord.gg/FWnQzgjr"
-                target="_blank"
-                @blur="
-                  setDiscordIcon('/img/social-icons/Discord-Symbol-White.svg')
-                "
-                @focus="
-                  setDiscordIcon('/img/social-icons/Discord-Symbol-Blurple.svg')
-                "
-                @mouseenter="
-                  setDiscordIcon('/img/social-icons/Discord-Symbol-Blurple.svg')
-                "
-                @mouseleave="
-                  setDiscordIcon('/img/social-icons/Discord-Symbol-White.svg')
-                ">
-                <img
-                  alt="Link to join the One Haven Discorg"
-                  class="social-icon"
-                  :src="discordIcon" />
-              </a>
-            </div>
-          </div>
+          <ContactFooter
+            :current-date="currentDate"
+            custom-class="is-displayed-desktop-flex-l margin-top-10px"
+            :discord-icon="discordIcon"
+            :instagram-icon="instagramIcon"
+            :set-discord-icon="setDiscordIcon"
+            :set-instagram-icon="setInstagramIcon" />
         </div>
         <div class="info-container">
           <div class="address-container">
@@ -132,62 +78,13 @@ const setInstagramIcon = (src: string) => {
             </ul>
           </div>
         </div>
-        <div class="is-displayed-mobile-flex-l footer-container">
-          <div>
-            <p class="footer-text">
-              {{ `© ${currentDate} One Haven. All rights reserved.` }}
-            </p>
-            <a class="footer-text" href="/privacy">Privacy Policy</a>
-          </div>
-          <div class="social-container">
-            <a
-              class="social-icon-container"
-              href="https://www.instagram.com/one_haven5956/"
-              target="_blank"
-              @blur="
-                setInstagramIcon('/img/social-icons/Instagram_Glyph_White.svg')
-              "
-              @focus="
-                setInstagramIcon(
-                  '/img/social-icons/Instagram_Glyph_Gradient.svg'
-                )
-              "
-              @mouseenter="
-                setInstagramIcon(
-                  '/img/social-icons/Instagram_Glyph_Gradient.svg'
-                )
-              "
-              @mouseleave="
-                setInstagramIcon('/img/social-icons/Instagram_Glyph_White.svg')
-              ">
-              <img
-                alt="Link to the One Haven Instagram"
-                class="social-icon"
-                :src="instagramIcon" />
-            </a>
-            <a
-              class="social-icon-container"
-              href="https://discord.gg/FWnQzgjr"
-              target="_blank"
-              @blur="
-                setDiscordIcon('/img/social-icons/Discord-Symbol-White.svg')
-              "
-              @focus="
-                setDiscordIcon('/img/social-icons/Discord-Symbol-Blurple.svg')
-              "
-              @mouseenter="
-                setDiscordIcon('/img/social-icons/Discord-Symbol-Blurple.svg')
-              "
-              @mouseleave="
-                setDiscordIcon('/img/social-icons/Discord-Symbol-White.svg')
-              ">
-              <img
-                alt="Link to join the One Haven Discorg"
-                class="social-icon"
-                :src="discordIcon" />
-            </a>
-          </div>
-        </div>
+        <ContactFooter
+          :current-date="currentDate"
+          custom-class="is-displayed-mobile-flex-l"
+          :discord-icon="discordIcon"
+          :instagram-icon="instagramIcon"
+          :set-discord-icon="setDiscordIcon"
+          :set-instagram-icon="setInstagramIcon" />
       </div>
     </div>
   </SectionSlot>
@@ -246,28 +143,6 @@ a {
   display: flex;
   justify-content: space-between;
 }
-.footer-text {
-  font-size: 14px;
-  margin: 0;
-}
-.copy-right-text {
-  padding-bottom: 5px;
-}
-.footer-container {
-  width: 100%;
-  justify-content: space-between;
-  align-items: center;
-  gap: 15px;
-}
-.social-container {
-  gap: 15px;
-  height: 100%;
-  display: flex;
-  align-items: center;
-}
-.social-icon {
-  height: 30px;
-}
 @media (max-width: 768px) {
   .contact-content-container {
     flex-direction: column;
@@ -276,9 +151,6 @@ a {
   .map-container,
   .info-container {
     width: 100%;
-  }
-  .social-container {
-    gap: 10px;
   }
 }
 @media (max-width: 480px) {
